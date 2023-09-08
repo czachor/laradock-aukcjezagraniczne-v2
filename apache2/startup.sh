071eb7f6b7246372fa31b34122d2f13fbeff4cff
czachor <f@czachorowski.pro>
2023-09-08 20:12:07 +0200

    Docker config (apache2 mysql phpmyadmin)

--- apache2/startup.sh
+++ apache2/startup.sh
@@ -8,7 +8,7 @@ if [ ${APACHE_HTTP2} = true ]; then
     openssl genrsa -out "/etc/apache2/ssl/ssl_site.key" 2048
     openssl rand -out /root/.rnd -hex 256
     openssl req -new -key "/etc/apache2/ssl/ssl_site.key" -out "/etc/apache2/ssl/ssl_site.csr" -subj "/CN=laradock.test/O=Laradock/C=BR"
-    openssl x509 -req -days 365 -extfile <(printf "subjectAltName=DNS:laradock.test,DNS:*.laradock.test") -in "/etc/apache2/ssl/ssl_site.csr" -signkey "/etc/apache2/ssl/ssl_site.key" -out "/etc/apache2/ssl/ssl_site.crt"
+    openssl x509 -req -days 365 -extfile <(printf "subjectAltName=DNS:localhost,DNS:*.localhost") -in "/etc/apache2/ssl/ssl_site.csr" -signkey "/etc/apache2/ssl/ssl_site.key" -out "/etc/apache2/ssl/ssl_site.crt"
   fi
 
   a2enmod rewrite
